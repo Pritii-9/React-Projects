3348471b661269ac21dc45c9e9ae9a2c5a55d7ab
Priti Jadhav <pvjadhav2513@gmail.com>
2025-02-03 06:54:04 +0530

    project-4 done

--- Project-4/server/src/index.ts
+++ Project-4/server/src/index.ts
@@ -1,64 +1,77 @@
+
 import express from "express";
 import path from "path";
 import cors from "cors";
+
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const corsOptions = {
+  origin: 'http://localhost:5173', // Allow requests from this origin
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  maxAge: 3600, // Cache CORS configuration for 1 hour
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 
-app.use("/images", express.static(path.join(__dirname, "../public/images")));
+
+const imagesDir = path.join(__dirname, "../public/images");
+app.use("/images", express.static(imagesDir));
+
+const foodData = [
+  {
+    name: "Boilded Egg",
+    price: 10,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    image: "http://localhost:9000/images/egg.png", // Updated image URL
+    type: "breakfast",
+  },
+  {
+    name: "RAMEN",
+    price: 25,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    image: "http://localhost:9000/images/ramen.png", // Updated image URL
+    type: "lunch",
+  },
+  {
+    name: "GRILLED CHICKEN",
+    price: 45,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    image: "http://localhost:9000/images/chicken.png",
+    type: "dinner",
+  },
+  {
+    name: "CAKE",
+    price: 18,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    image: "http://localhost:9000/images/cake.png",
+    type: "breakfast",
+  },
+  {
+    name: "BURGER",
+    price: 23,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    image: "http://localhost:9000/images/burger.png",
+    type: "lunch",
+  },
+  {
+    name: "PANCAKE",
+    price: 25,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    image: "http://localhost:9000/images/pancake.png",
+    type: "dinner",
+  },
+];
+
+
 
 app.get("/", (req, res) => {
-  console.log(path.join(__dirname, "../public"));
-  const foodData = [
-    {
-      name: "Boilded Egg",
-      price: 10,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      image: "/images/egg.png",
-      type: "breakfast",
-    },
-    {
-      name: "RAMEN",
-      price: 25,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      image: "/images/ramen.png",
-      type: "lunch",
-    },
-    {
-      name: "GRILLED CHICKEN",
-      price: 45,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      image: "/images/chicken.png",
-      type: "dinner",
-    },
-    {
-      name: "CAKE",
-      price: 18,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      image: "/images/cake.png",
-      type: "breakfast",
-    },
-    {
-      name: "BURGER",
-      price: 23,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      image: "/images/burger.png",
-      type: "lunch",
-    },
-    {
-      name: "PANCAKE",
-      price: 25,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      image: "/images/pancake.png",
-      type: "dinner",
-    },
-  ];
-
   res.json(foodData);
 });
 
-app.listen(9000, () => {
-  console.log("Server is running on port 9000");
+const port = 9000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
